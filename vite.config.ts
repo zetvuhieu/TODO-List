b41137952fb3c0910fff848782a2cc7d7ffc33f1
Nguyen Vu Hieu <2405vuhieuzet@gmail.com>
2024-08-29 12:45:47 +0700

    up 3

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist",
-  },
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "src"),
-    },
-  },
 });
