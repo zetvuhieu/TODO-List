457707fd7b9786dd661d6bdff2cfb26cac5382c5
Nguyen Vu Hieu <2405vuhieuzet@gmail.com>
2024-08-29 12:51:20 +0700

    up 12

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
+import path from "path";
+import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
